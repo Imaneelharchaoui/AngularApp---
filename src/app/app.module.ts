671d612a4e21339fb4996ab35a968b925f1f2d60
imaneeeh <46714956+Imaneelharchaoui@users.noreply.github.com>
2020-10-17 19:40:54 +0100

    Angular is the best

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: '', component: LoginComponent},
+      {path: 'he', component: WelcomeComponent},  {path: 'crit', component: LoginComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
